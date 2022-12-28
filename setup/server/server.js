efd140c9c62348c977c607ec046d0ab5eed485d6
edielam <edguard738@gmail.com>
2022-12-28 15:36:40 +0000

    layout setup

--- setup/server/server.js
+++ setup/server/server.js
@@ -1,3 +1,5 @@
 import express from 'express';
+import devBundle from './devBundle';
 
 const app = express();
+devBundle.compile(app); // this is only for dev't mode. Should be commented out in production
