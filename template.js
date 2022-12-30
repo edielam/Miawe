a77d59c822cdb4e8da9205d68453fac95ff7d09a
edielam <edguard738@gmail.com>
2022-12-30 12:42:57 +0000

    miawe setup

--- template.js
+++ template.js
@@ -4,6 +4,8 @@ export default () => {
     <head>
     <meta charset="utf-8">
     <title>MERN Kickstart</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     </head>
     <body>
     <div id="root"><h1>Hello World</h1></div>
