02110580e3091139b79107a97945ddf44c9a44de
edielam <edguard738@gmail.com>
2023-07-31 21:48:59 +0000

    version 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# Miawe
- A social media application inspired by Twitter and Facebook. Features include post sharing, liking and commenting, following friends, and a news feed. Miawe is a word from the African language called Ewe which translates as "We are the ones".
---
+++ miawe1.0/template.js
@@ -0,0 +1,15 @@
+export default () => {
+    return `<!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>MERN Kickstart</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    </head>
+    <body>
+        <div id="root"></div>
+        <script type="text/javascript" src="/dist/bundle.js"></script>
+    </body>
+    </html>`
+   }
